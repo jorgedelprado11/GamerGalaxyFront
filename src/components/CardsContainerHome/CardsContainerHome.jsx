2412ec1cabf6f1c1955387562c69ce4013e98ba6
jorgedelprado11 <jorgedelprado11@gmail.com>
2023-09-03 19:48:54 -0300

    luquita adentro

--- src/components/CardsContainerHome/CardsContainerHome.jsx
+++ src/components/CardsContainerHome/CardsContainerHome.jsx
@@ -1,22 +1,23 @@
-/** @format */
 import { Card } from "../CardHome/CardHome";
+import { Link } from "react-router-dom";
 
 export const CardsContainer = ({ descuentos, indexCards, cardsUno }) => {
   return (
-    <div
-      className="grid grid-cols-5 h-full gap-2 p-12 bg-cover bg-center w-screen"
-      style={{
-        backgroundImage:
-          'url("https://images8.alphacoders.com/595/595000.jpg")',
-        backgroundBlendMode: "multiply", // Para mejorar el contraste de la imagen
-        backgroundColor: "rgba(0, 0, 0, 0.6)",
-      }}
-    >
-      {descuentos
-        ?.map((producto) => {
-          return <Card producto={producto} key={producto.id} />;
-        })
-        .slice(cardsUno, indexCards)}
+    <div className="h-3/5 ">
+      <div className="grid grid-cols-5 p-20 ml-10">
+        {descuentos
+          ?.map((producto) => {
+            return <Card producto={producto} key={producto.id} />;
+          })
+          .slice(cardsUno, indexCards)}
+      </div>
+      <div className="m-12 absolute inset-x-0 top-0 flex items-center justify-center z-10">
+        <Link to="/productos" className="no-underline border-none">
+          <h1 className="text-4xl font-bold text-white shadow-xl border-0 ">
+            PRODUCTOS DESTACADOS
+          </h1>
+        </Link>
+      </div>
     </div>
   );
 };
