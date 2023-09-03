30abd32f0cba85bac29c6214c969e5e48d382287
Lucas Adragna <adragnajoaco@gmail.com>
2023-09-03 17:42:49 -0300

    Modificando Home

--- src/components/CardsContainer/CardsContainer.jsx
+++ src/components/CardsContainer/CardsContainer.jsx
@@ -1,15 +1,18 @@
+/** @format */
+
 import Card from "../Card/Card";
 
 const CardsContainer = ({ productos }) => {
   // console.log("cardscontainer", productos);
   // console.log('desde la cardssss',productos);
- 
+
   return (
     <div className="grid grid-cols-3">
-      {productos?.map((producto) => (
-        {},
-        <Card key={producto.id_producto} producto={producto} />
-      ))}
+      {productos?.map(
+        (producto) => (
+          {}, (<Card key={producto.id_producto} producto={producto} />)
+        )
+      )}
     </div>
   );
 };
--- src/components/CardsContainerHome/CardsContainerHome.jsx
+++ src/components/CardsContainerHome/CardsContainerHome.jsx
@@ -3,20 +3,19 @@ import { Card } from "../CardHome/CardHome";
 
 export const CardsContainer = ({ descuentos, indexCards, cardsUno }) => {
   return (
-    <div
-      className="grid grid-cols-5 h-full gap-2 p-12 bg-cover bg-center w-screen"
-      style={{
-        backgroundImage:
-          'url("https://images8.alphacoders.com/595/595000.jpg")',
-        backgroundBlendMode: "multiply", // Para mejorar el contraste de la imagen
-        backgroundColor: "rgba(0, 0, 0, 0.6)",
-      }}
-    >
-      {descuentos
-        ?.map((producto) => {
-          return <Card producto={producto} key={producto.id} />;
-        })
-        .slice(cardsUno, indexCards)}
+    <div className="h-3/5 ">
+      <div className="grid grid-cols-5 p-20 ml-10">
+        {descuentos
+          ?.map((producto) => {
+            return <Card producto={producto} key={producto.id} />;
+          })
+          .slice(cardsUno, indexCards)}
+      </div>
+      <div className="m-12 absolute inset-x-0 top-0 flex items-center justify-center z-10">
+        <h1 className="text-4xl font-bold text-slate-900 text-decoration-line:4px underline transform">
+          <span className="text-gray-400 shadow-xl">PRODUCTOS DESTACADOS</span>
+        </h1>
+      </div>
     </div>
   );
 };
